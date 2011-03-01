fda89c4e5567090c1633d53dbce0ed73a9fb4e81
michael.nischt <michael.nischt@localhost>
2011-03-01 18:27:57 +0100

    fixed download.php header

--- tools/www/shift/obj/download.php
+++ tools/www/shift/obj/download.php
@@ -1,47 +1,35 @@
 <?php
-  echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-  echo "<!DOCTYPE html>";
+  if(isset($_POST["downloadfile"]) && file_exists($file = $_POST["downloadfile"]))
+  {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    exit;
+  }
+  else
+  {
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    echo "<!DOCTYPE html>";      
 ?>
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <title>ObjShift: Download</title>
 </head>
 <body>
-
-<?php
-  if(isset($_POST["downloadfile"]))
-  {
-    $file=$_POST["downloadfile"];
-    echo $file;
-    
-    if (file_exists($file)) 
-    {
-      header('Content-Description: File Transfer');
-      header('Content-Type: application/octet-stream');
-      header('Content-Disposition: attachment; filename='.basename($file));
-      header('Content-Transfer-Encoding: binary');
-      header('Expires: 0');
-      header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-      header('Pragma: public');
-      header('Content-Length: ' . filesize($file));
-      ob_clean();
-      flush();
-      readfile($file);
-      exit;
-    }
-    else
-    {
-      echo " does not exist";
-    }
+  error
+</body>
+</html>  
+<?php  
   }
-  else
-  {
-    echo "error";
-  }
-
 ?>
 
   
-  </body>
-</html>
+
