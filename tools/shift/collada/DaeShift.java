35c1f57c53fe5a0116f24fa2dcc76deae9e97229
michael.nischt <michael.nischt@localhost>
2011-03-03 23:10:39 +0100

    Wire and util classes for DaeShift

--- tools/shift/collada/DaeShift.java
+++ tools/shift/collada/DaeShift.java
@@ -30,7 +30,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
-package org.interaction3d.assembly.tools.shift.obj;
+package org.interaction3d.assembly.tools.shift.collada;
 
 import java.io.BufferedReader;
 import java.io.Closeable;
@@ -42,14 +42,28 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import org.interaction3d.assembly.tools.shift.util.Path;
+
 
 /**
  *
  * @author Michael Nischt
  */
-public final class ObjShift 
+public final class DaeShift 
 { 
 	private static final String USAGE;
 	static
@@ -58,16 +72,17 @@ public final class ObjShift
 		usage += "    Required argument missing: source-file\n\n";
 		
 		usage += "USAGE:\n";
-		usage += "    java -jar ObjShift.jar [-m] <source-file> [output-dir]\n";
+//		usage += "    java -jar DaeShift.jar [-m] <source-file> [output-dir]\n";
+//		usage += "    java -jar DaeShift.jar <source-file> [output-dir]\n";
 		usage += "\n\n";
 		
-		usage += "    Where:\n\n";
-		usage += "        -m, --multiple\n";
-		usage += "         creates separate(multiple) mesh files for each object\n";
-		usage += "         note that this applies to o(object name) but not g (group names)\n";
+//		usage += "    Where:\n\n";
+//		usage += "        -m, --multiple\n";
+//		usage += "         creates separate(multiple) mesh files for each object\n";
+//		usage += "         note that this applies to o(object name) but not g (group names)\n";
 		
 		usage += "\n\n\n";		
-		usage += "    ObjShift - Converter tool for .obj files\n";
+		usage += "    DaeShift - Converter tool for .dae (collada) files\n";
 		
 		USAGE = usage;
 	}
@@ -114,54 +129,54 @@ public final class ObjShift
 				dst = System.getProperty("user.dir");		
 		}
 				    
-		ObjShift.convert(src, dst, multiple);
+		DaeShift.convert(src, dst);
 	}
 
-	public static void convert(String inputFile, String outputDir, boolean multiple) throws IOException
+	public static void convert(String inputFile, String outputDir) throws Exception
 	{
 	  File file = Path.file(outputDir);        
 	  if(!file.isDirectory()) { file.mkdirs(); }
 	  
 		String name = Path.trunkObj(Path.filename(inputFile));                
-	  ObjProcessor proc = new ObjProcessor(name, multiple);
+//	  DaeProcessor proc = new DaeProcessor(name, multiple);
 	  
 	  // load
-		readObj(proc, inputFile);
+		readDae(inputFile);
 	  
 	  // save
 	  final String path = outputDir;
-	  Mesh.Assembly assembly = new Mesh.Assembly() 
-    {
-    		public void assemble(String name, CharSequence xml, ByteBuffer data)
-    		{
-    			try 
-    			{
-    				writeXml(xml, path + File.separator + name + ".mesh.xml");
-	          writeData(data, path + File.separator + name + ".mesh.dat");			
-          }
-          catch(IOException ioe) { throw new RuntimeException(ioe); }
-    		}
-    };
-	  
-	  for(Mesh mesh : proc.getMeshes())
-	  {            	  	
-	    mesh.convert(assembly);
-	  }
+//	  Mesh.Assembly assembly = new Mesh.Assembly() 
+//    {
+//    		public void assemble(String name, CharSequence xml, ByteBuffer data)
+//    		{
+//    			try 
+//    			{
+//    				writeXml(xml, path + File.separator + name + ".mesh.xml");
+//	          writeData(data, path + File.separator + name + ".mesh.dat");			
+//          }
+//          catch(IOException ioe) { throw new RuntimeException(ioe); }
+//    		}
+//    };
+//	  
+//	  for(Mesh mesh : proc.getMeshes())
+//	  {            	  	
+//	    mesh.convert(assembly);
+//	  }
 	}
 	
-	private static void readObj(ObjProcessor proc, String inputFile) throws IOException
+	private static void readDae(String inputFile) throws Exception
 	{
-	  BufferedReader reader = null;
-	  try
-	  { 
-	  	reader = new BufferedReader(new InputStreamReader(Path.inputStream(inputFile))); 
-			String line;
-			while((line=reader.readLine()) != null)
-			{
-			    proc.line(line);
-			}
-		} 
-	  finally { close(reader); }    	
+    DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+    domFactory.setNamespaceAware(false);
+    DocumentBuilder builder = domFactory.newDocumentBuilder();
+
+    Document document = builder.parse(inputFile);
+    XPath xpath = XPathFactory.newInstance().newXPath();
+    
+    String version = (String) xpath.compile("COLLADA/@version").evaluate(document, XPathConstants.STRING);
+    System.out.println("Collada version: " + version);
+    
+    MeshProcessor meshProcessor = new MeshProcessor(document, xpath);
 	}
 
 	private static void writeXml(CharSequence xml, String outputFile) throws IOException
@@ -198,5 +213,5 @@ public final class ObjShift
 		  }
 	}    
 
-	private ObjShift() { /* static class */ }
+	private DaeShift() { /* static class */ }
 }
