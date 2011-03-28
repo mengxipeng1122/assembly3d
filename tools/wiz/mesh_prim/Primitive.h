263cfed63f04d0ac6e2c76c9b992cd74e33182c2
Michael Nischt <Michael.Nischt@gmail.com>
2011-03-28 14:39:39 +0000

    comment labels

--- tools/wiz/mesh_prim/Primitive.h
+++ tools/wiz/mesh_prim/Primitive.h
@@ -62,11 +62,11 @@ namespace assembly3d
                             // x - right
                             // y -  up
                             //
-                            //b --- c
+                            //a --- b
                             //|     |
-                            //| CCW |
+                            //|  CW |
                             //|     |
-                            //a --- d
+                            //d --- c
                             unsigned int start = stack * (m_slices + 1);
                             unsigned int a = start + slice;
                             unsigned int b = a + 1;
